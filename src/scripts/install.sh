66dafec43c21736ed697380ad78311390b39fe2e
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-10-20 13:48:09 -0700

    refactor: remove scripts from YAML (#50)
    * ci: remove exclusions and update orb tools
    
    * ci: replace old context
    
    * chore: remove orb tools from imports
    
    * refactor: extract `initialize` sh to its own file
    
    * refactor: extract `install` sh to its own file
    
    * refactor: remove unused variable
    
    * fix: add check for failed cd
    
    * ci: bump orb tools

---
+++ src/scripts/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+install () {
+  # Set sudo to work whether logged in as root user or non-root user
+  if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+  cd ~/ || return 1
+  curl -Ss --retry 5 https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-"$ORB_VAL_VERSION"-linux-x86_64.tar.gz | tar xz
+  echo 'source ~/google-cloud-sdk/path.bash.inc' >> "$BASH_ENV"
+}
+
+if grep 'docker\|lxc' /proc/1/cgroup > /dev/null 2>&1; then
+  if [[ $(command -v gcloud) == "" ]]; then
+    install
+  else
+    echo "gcloud CLI is already installed."
+  fi
+else
+  echo "----------------------------------------------------------------------------------------------------"
+  echo "this is a machine executor job, replacing default installation of gcloud CLI"
+  echo "----------------------------------------------------------------------------------------------------"
+  sudo rm -rf /opt/google-cloud-sdk
+  install
+fi
