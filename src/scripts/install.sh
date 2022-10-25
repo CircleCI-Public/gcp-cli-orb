674d8f50dc3464e77c28dc8ad2cbe81a5fbb08a2
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-10-25 10:51:17 -0700

    feat: improve version handling (#51)
    * feat: change default version to latest
    
    This commit also changes how the orb handles installation.
    
    Now, it doesn't rely on executor to decide if it's going to uninstall existing versions.
    
    Existing versions are simply uninstalled if they don't match the requested version.
    
    * feat: bump machine image version
    
    * refactor: improve parameter's description
    
    * ci: improve tests and add alpine
    
    * ci: remove `363.0.0` to avoid problems with newer python versions
    
    * feat: add check for `curl`
    
    * ci: install curl for alpine tests
    
    * feat: remove unnecessary step from `install`
    
    * ci: replace alpine with python-alpine
    
    * feat: improve post install message
    
    * feat: add alpine warning
    
    * ci: create $BASH_ENV in pre-steps
    
    * ci: add checks in tests
    
    * ci: install sudo in pre-steps of tests
    
    * fix: sudo, or lack of, breaking commands
    
    * ci: remove pre-steps from machine job
    
    * ci: improve check and move them to workflow
    
    * ci: add tests to requirements
    
    * ci: remove old tests
    
    * refactor: make code cleaner
    
    * refactor: improve alpine message
    
    * refactor: grammar
    
    * refactor: fix formatting

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,23 +1,106 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+# shellcheck disable=SC3043 # while "local" isn't POSIX, it's supported in many shells. See: https://www.shellcheck.net/wiki/SC3043
 
-install () {
-  # Set sudo to work whether logged in as root user or non-root user
-  if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
-  cd ~/ || return 1
-  curl -Ss --retry 5 https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-"$ORB_VAL_VERSION"-linux-x86_64.tar.gz | tar xz
-  echo 'source ~/google-cloud-sdk/path.bash.inc' >> "$BASH_ENV"
+home="$(printf '%s\n' "$HOME" | sed 's/\//\\\//g')"
+readonly home
+
+base_dir="$(printf '%s\n' "$CIRCLE_WORKING_DIRECTORY" | sed "s/~/$home/")"
+readonly base_dir
+
+fetch_latest_version() {
+  local release_notes
+  local release_notes_exit_code
+
+  release_notes="$(curl --location --silent --fail --retry 3 https://cloud.google.com/sdk/docs/release-notes)"
+  release_notes_exit_code="$?"
+
+  [ "$release_notes_exit_code" -gt 0 ] && printf '%s\n' "Failed to get release notes" && return "$release_notes_exit_code"
+
+  local releases
+  releases="$(printf '%s\n' "$release_notes" | grep -E '<h2 id=".*" data-text=".*">[0-9]+.[0-9]+.[0-9]+.*</h2>' | sed 's/<h2.*>\([0-9]*.[0-9]*.[0-9]*\).*<\/h2>/\1/')"
+
+  local latest_version
+  latest_version="$(printf '%s\n' "$releases" | head -n 1)"
+
+  [ -z "$latest_version" ] && printf '%s\n' "Couldn't find out what is the latest version available." && return 1
+  version="$latest_version"
 }
 
-if grep 'docker\|lxc' /proc/1/cgroup > /dev/null 2>&1; then
-  if [[ $(command -v gcloud) == "" ]]; then
-    install
+# $1: version
+install() {
+  local arg_version="$1"
+  [ -z "$arg_version" ] && printf '%s\n' "No version provided." && return 1
+
+  cd "$base_dir" || return 1
+  curl --location --silent --fail --retry 3 https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-"$arg_version"-linux-x86_64.tar.gz | tar xz
+  printf '%s\n' ". $base_dir/google-cloud-sdk/path.bash.inc" >> "$BASH_ENV"
+
+  # If the envinronment is Alpine, remind the user to source $BASH_ENV in every step.
+  if [ -f /etc/os-release ] && grep -q "Alpine" "/etc/os-release"; then
+    printf '%s\n' "Alpine detected. Please make sure to source \$BASH_ENV in every step."
+    printf '%s\n' "Otherwise gcloud won't be available."
+    printf '%s\n' "You can do this by adding the following line in the beginning of your command:"
+    printf '%s\n' "\". \$BASH_ENV\""
+
+    # Alpine also needs a workaround since Google's "path.bash.inc" doesn't work.
+    printf '%s\n' "export PATH=$base_dir/google-cloud-sdk/bin:$PATH" >> "$BASH_ENV"
+  fi
+
+  # shellcheck disable=SC1090
+  . "$BASH_ENV"
+  if ! command -v gcloud > /dev/null 2>&1; then return 1; fi
+  printf '%s\n' "Google Cloud SDK version: $(gcloud --version)"
+}
+
+uninstall() {
+  if ! command -v sudo > /dev/null 2>&1; then
+    printf '%s\n' "sudo is required to uninstall the Google Cloud SDK."
+    printf '%s\n' "Please install it and try again."
+    return 1
+  fi
+
+  # Set sudo to work whether logged in as root user or non-root user.
+  if [ "$(id -u)" -eq 0 ]; then sudo=""; else sudo="sudo"; fi
+
+  local installation_directory
+  installation_directory="$(gcloud info --format='value(installation.sdk_root)')"
+
+  local config_directory
+  config_directory="$(gcloud info --format='value(config.paths.global_config_dir)')"
+
+  # shellcheck disable=SC2086 # $sudo is not a variable, it's a command.
+  $sudo rm -rf "$installation_directory" || return 1
+
+  # shellcheck disable=SC2086 # $sudo is not a variable, it's a command.
+  $sudo rm -rf "$config_directory" || return 1
+}
+
+# Check if curl is installed
+if ! command -v curl > /dev/null 2>&1; then
+  printf '%s\n' "curl is required to install the Google Cloud SDK."
+  printf '%s\n' "Please install it and try again."
+  exit 1
+fi
+
+# Figure out what is latest version available if "latest" is passed as an argument.
+version="$ORB_VAL_VERSION"
+[ "$version" = "latest" ] && fetch_latest_version
+
+if command -v gcloud > /dev/null 2>&1; then
+  installed_version="$(gcloud version | head -n 1 | sed 's/Google Cloud SDK \([0-9]*.[0-9]*.[0-9]*\)/\1/')"
+
+  if [ "$installed_version" != "$version" ]; then
+    printf '%s\n' "The version installed ($installed_version) differs from the version requested ($version)."
+    printf '%s\n' "Uninstalling v${installed_version}..."
+    if ! uninstall; then printf '%s\n' "Failed to uninstall the current version."; exit 1; fi
+    
+    printf '%s\n' "Installing v${version}..."
+    if ! install "$version"; then printf '%s\n' "Failed to install the requested version."; exit 1; fi
   else
-    echo "gcloud CLI is already installed."
+    printf '%s\n' "The version installed ($installed_version) matches the version requested ($version)."
+    printf '%s\n' "Skipping installation."
   fi
 else
-  echo "----------------------------------------------------------------------------------------------------"
-  echo "this is a machine executor job, replacing default installation of gcloud CLI"
-  echo "----------------------------------------------------------------------------------------------------"
-  sudo rm -rf /opt/google-cloud-sdk
-  install
+  printf '%s\n' "Google Cloud SDK is not installed. Installing it."
+  if ! install "$version"; then printf '%s\n' "Failed to install the requested version."; exit 1; fi
 fi
