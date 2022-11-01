bacf6bd97a1de4140a25c8fa973c569968c868af
Jaryt <jarytbustard@gmail.com>
2022-11-01 17:04:28 -0400

    fix: eval file paths

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -6,6 +6,10 @@ readonly project_id=${!ORB_ENV_PROJECT_ID}
 readonly compute_zone=${!ORB_ENV_COMPUTE_ZONE}
 readonly compute_region=${!ORB_ENV_COMPUTE_REGION}
 
+# Eval parameters
+oidc_file_path=$(eval "echo $ORB_EVAL_OIDC_FILE")
+cred_file_path=$(eval "echo $ORB_EVAL_CRED_FILE")
+
 # Store service account
 printf '%s\n' "$service_key" > "$HOME"/gcloud-service-key.json
 
@@ -19,18 +23,18 @@ gcloud --quiet config set component_manager/disable_update_check true
 # Determine credential source file by checking if an ENV exists as a file or a string.
 # If it's a file, then use it, or if it's a string then base64 decode it.
 
-if [ -n "$ORB_VAL_OIDC_FILE" ]; then
+if [ -n "$oidc_file_path" ]; then
   # Store OIDC token in temp file
   gcloud iam workload-identity-pools create-cred-config \
       "projects/${!ORB_ENV_PROJECT_ID}/locations/global/workloadIdentityPools/${!ORB_ENV_POOL_ID}/providers/${!ORB_ENV_POOL_PROVIDER_ID}" \
-      --output-file="$ORB_VAL_CRED_FILE"  \
+      --output-file="$cred_file_path"  \
       --service-account="${!ORB_ENV_SERVICE_EMAIL}" \
-      --credential-source-file="$ORB_VAL_OIDC_FILE"
+      --credential-source-file="$oidc_file_path"
 
   # Configure gcloud to leverage the generated credential configuration
-  gcloud auth login --brief --cred-file "$ORB_VAL_CRED_FILE"
+  gcloud auth login --brief --cred-file "$cred_file_path"
   # Configure ADC
-  echo "export GOOGLE_APPLICATION_CREDENTIALS='$ORB_VAL_CRED_FILE'" | tee -a "$BASH_ENV"
+  echo "export GOOGLE_APPLICATION_CREDENTIALS='$cred_file_path'" | tee -a "$BASH_ENV"
 else
   gcloud auth activate-service-account --key-file="$HOME"/gcloud-service-key.json
 fi
