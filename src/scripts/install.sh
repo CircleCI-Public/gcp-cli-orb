9fb7e1c422d2a90a686fd770644f0e3a70670842
David Alberto Montaño Fetecua <david.montano@circleci.com>
2024-08-27 22:11:15 -0500

    fix: wrapping gcloud

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -118,6 +118,7 @@ download_with_retry() {
 create_wrappers() {
   local download_directory="$1"
   cat <<EOF > /c/Users/circleci/AppData/Local/Microsoft/WindowsApps/gcloud
+#! /usr/bin/bash
   bash -c "$download_directory/google-cloud-sdk/bin/gcloud \$@"
 EOF
 }
